5de2edc5369f1c932059736c030d151acd77773a
Hamza Benarfa <hamzabenarfa4@gmail.com>
2024-02-17 16:12:12 +0100

    added user table , corrected folder names

---
+++ backend/src/controller/user.js
@@ -0,0 +1,31 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+const createUser = async (userData) => {  
+    try {
+        const newUser = await prisma.user.create({
+            data: userData
+        });
+        return newUser;
+    } catch (error) {
+        console.error('Error creating user:', error);
+        throw error;
+    }
+};  
+
+const getUser = async (userData) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                email: userData.email
+            }
+        });
+        return user;
+    } catch (error) {
+        console.error('Error getting user:', error);
+        throw error;
+    }
+};
+
+module.exports = {createUser , getUser};
